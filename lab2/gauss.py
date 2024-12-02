77973667f35c22cc2f241c9173e32ed2cccf1f49
grysj <jgrys17@gmail.com>
2024-12-03 00:52:03 +0100

    nienawidze - lab3 mostly done

--- lab2/gauss.py
+++ lab2/gauss.py
@@ -3,15 +3,22 @@ from lu import lu_factorization
 from util import Number, random_matrix, matrix_partitions
 from matinv import invert_matrix
 from binet import binet
-from strassen import Strassen
+from strassen import Strassen, SMU
 
 def gauss_elimination(A: np.ndarray,b: np.ndarray, mult) -> np.ndarray:
+
+    if A.shape[0] == 1:
+        return b / A[0, 0]
 
     A11, A12,A21, A22 = matrix_partitions(A)
     n = A.shape[0]//2
 
-    b1 = b[:n]
-    b2 = b[n:]
+
+    # b1 = b[:n]
+    # b2 = b[n:]
+    b1 = b[:n].reshape(-1, 1)
+    b2 = b[n:].reshape(-1, 1)
+
     
     L11, U11 = lu_factorization(A11, mult=mult)
 
@@ -33,7 +40,7 @@ def gauss_elimination(A: np.ndarray,b: np.ndarray, mult) -> np.ndarray:
 
     x1 = mult(invert_matrix(U11, mult=mult), RHS1 - mult(C12, x2))
 
-    x = np.concatenate((x1, x2))
+    x = np.concatenate((x1, x2)).flatten()
 
 
     return x
@@ -52,8 +59,11 @@ if __name__ == '__main__':
     A = np.random.rand(n, n)
     b = np.random.rand(n)
 
+    print(A, b)
+
     # Solve using recursive Gaussian elimination
-    x_recursive = gauss_elimination(A, b, binet)
+    x_recursive = gauss_elimination(A, b, SMU)
+    print(x_recursive)
 
     # Solve using numpy's built-in function
     x_numpy = np.linalg.solve(A, b)
